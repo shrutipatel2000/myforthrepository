1a7d5ac89a98d9c17fc602915fb6e631dbb9cbed
shruti patel <Shrutipatel203@gmail.com>
2023-10-31 19:05:58 +0000

    myforthcommit

---
+++ FloatClass.java
@@ -0,0 +1,16 @@
+package FloatPackage;
+
+public class FloatClass {
+
+    public static void main(String[]args){
+        float a=60;
+        float b=50;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
